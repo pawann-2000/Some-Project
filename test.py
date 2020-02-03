3bfaed488140ba721f24c61cd1daa8a592e1e4d6
pawan-py <pawann931@gmail.com>
2020-02-03 20:11:20 +0530

    added a test file

---
+++ test.py
@@ -0,0 +1,22 @@
+# Create an @authenticated decorator that only allows the function to run is user1 has 'valid' set to True:
+user1 = {
+    'name': 'Sorna',
+    # changing this will either run or not run the message_friends function.
+    'valid': True
+}
+
+
+def authenticated(fn):
+    for val in user1.values():
+        if(val == True):
+            @authenticated
+        else:
+            return False
+
+
+@authenticated
+def message_friends(user1):
+    print('message has been sent')
+
+
+message_friends(user1)
